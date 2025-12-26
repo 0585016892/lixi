85780fce6961810fa424c06e267d503e2227e562
0585016892 <tranhung6829@gmail.com>
2025-12-26 15:12:27 +0700

    first commit

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "lixi-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./anh-tien-50k.jpg",
+  "./nhac-tet.mp3"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
